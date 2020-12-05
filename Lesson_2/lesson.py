41e7e7f6d30298d903342a22f9250c7d9b0585d8
Viacheslav Gurov <gurov>
2020-12-05 01:20:00 -0500

    dt 05 dec 2020

---
+++ Lesson_2/lesson.py
@@ -0,0 +1,29 @@
+from general import Transport
+
+class Ship(Transport):
+    def swim(self):
+        print("I can swim")
+
+class Aircraft(Transport):
+    def fly(self):
+        print("I can fly")
+
+    def speed_up(self):
+        self.speed += 5
+        self.noise += 2
+
+boat = Ship("Speedboat")
+boat2 = Ship("Nameboat")
+plane = Aircraft("Boeing")
+
+boat.say_name()
+boat2.say_name()
+plane.say_name()
+
+print(plane.get_speed(), plane.get_noise())
+plane.speed_up()
+print(plane.get_speed(), plane.get_noise())
+plane.set_speed(100)
+print(plane.get_speed(), plane.get_noise())
+
+
